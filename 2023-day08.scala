a21d31389697c214445b7008fc8d036b69f6e739
Jamie Thompson <bishbashboshjt@gmail.com>
2023-12-08 09:58:49 +0100

    cleanup day 8

--- 2023-day08.scala
+++ 2023-day08.scala
@@ -3,53 +3,43 @@ package day08
 import scala.language.experimental.namedTuples
 import regexglob.RegexGlobbing.*
 
-type Network = (path: LazyList[Char], nodes: Map[String, (String, String)])
+type Node = (left: String, right: String)
+type Network = (path: LazyList[Char], nodes: Map[String, Node])
+type Filter = String => Boolean
 
 def parse(input: String): Network =
   val r"$lr\n\n${r"$keys = ($ls, $rs)"}...(\n)" = input: @unchecked
-  val nodes = keys.lazyZip(ls).lazyZip(rs).map((k, l, r) =>
-    k -> (l, r)).toMap
+  val nodes = keys.lazyZip(ls.zip(rs)).map((k, v) => k -> v).toMap
   (path = LazyList.continually(lr).flatten, nodes = nodes)
 
-def part1(input: String): Long =
+def search(starts: Seq[String], isEnd: Filter, network: Network): Seq[Int] =
+  val state = starts.map(_ -> (steps = 0, done = false)).toMap
+  val states = LazyList.unfold((network.path, state)):
+    (next, state) =>
+      if state.values.forall(_.done) then None
+      else
+        val state0 = state.map: (key, path) =>
+          if !path.done then
+            val node = network.nodes(key)
+            val key0 = if next.head == 'L' then node.left else node.right
+            key0 -> (steps = path.steps + 1, done = isEnd(key0))
+          else key -> path
+        Some(state0 -> (next.tail, state0))
+  states.last.values.map(_.steps).toSeq
+
+def lcm(ns: Iterable[Int]): BigInt =
+  ns.foldLeft(BigInt(1)): (acc, n) =>
+    val g = acc.gcd(n)
+    acc * n / g
+
+def part1(input: String): Int =
   val network = parse(input)
-  val initial = (key = "AAA", next = network.path, steps = 0L)
-  val stepss = LazyList.unfold(initial): state =>
-    if state.key == "ZZZ" then None
-    else
-      val dir = state.next.head
-      val (l, r) = network.nodes(state.key)
-      val key0 = if dir == 'L' then l else r
-      val state0 = (key = key0, next = state.next.tail, steps = state.steps + 1L)
-      Some(state.steps + 1L -> state0)
-  stepss.last
+  search(Seq("AAA"), _ == "ZZZ", network).last
 
 def part2(input: String): BigInt =
   val network = parse(input)
-  val initialKeys = network.nodes.keys.filter(_.endsWith("A")).toList
-  val initialSteps = initialKeys.map(_ -> Left(0L).withRight[Long]).toMap
-  val initial = (next = network.path, steps = initialSteps)
-  val stepss = LazyList.unfold(initial): state =>
-    if state.steps.values.forall(_.isRight) then None
-    else
-      val dir = state.next.head
-      val keys0 = state.steps.map: (key, value) =>
-        value match
-          case Left(steps) =>
-            val steps0 = steps + 1L
-            val (l, r) = network.nodes(key)
-            val key0 = if dir == 'L' then l else r
-            if key0.endsWith("Z") then key0 -> Right(steps0)
-            else key0 -> Left(steps0)
-          case _ => key -> value
-      val state0 = (next = state.next.tail, steps = keys0)
-      Some(keys0 -> state0)
-  val all = stepss.last.values.collect { case Right(steps) => steps }
-  def lcm(ns: Iterable[Long]) =
-    ns.foldLeft(BigInt(1)): (acc, n) =>
-      val g = acc.gcd(n)
-      acc * n / g
-  lcm(all)
+  val starts = network.nodes.keys.filter(_.endsWith("A")).toList
+  lcm(search(starts, _.endsWith("Z"), network))
 
 import challenges.*
 
